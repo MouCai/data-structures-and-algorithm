676d5399a4dc58bbd4dcb6230cec22b6019527ae
韦捷 <weijiewow@163.com>
2016-03-18 02:05:06 +0800

    add DoublyLinkedList/DoublyLinkedList.h

---
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,28 @@
+#ifndef _DOUBLY_LINKED_LIST_
+  #define _DOUBLY_LINKED_LIST_ 1;
+
+  struct List {
+     struct MNode *Head;
+     struct MNode *Tail;
+     struct MNode *LastNode;
+  };
+  
+  struct MNode {
+     struct MNode *Succ;
+     struct MNode *Pred;
+  };
+  
+  typedef struct MNode *NODE;
+  typedef struct List *LIST;
+  
+  LIST NewList(void);
+  NODE GetTail(LIST);
+  NODE GetHead(LIST);
+  NODE Append(LIST, NODE);
+  NODE Prepend(LIST, NODE);
+  NODE DeleteHead(LIST);
+  NODE DeleteTail(LIST);
+  NODE InsertAfter(LIST, NODE, NODE);
+  NODE DeleteNode(LIST, NODE);
+
+#endif
