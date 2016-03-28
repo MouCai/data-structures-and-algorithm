e8127cdada98c5b087303ff553fdac4ebe96aea9
韦捷 <weijiewow@163.com>
2016-03-28 19:27:43 +0800

    init set

---
+++ set/set.h
@@ -0,0 +1,29 @@
+#ifndef _SET_
+  #define _SET_
+  #include <stdlib.h>
+  typedef struct _SetElm_ {
+    void *data;
+    struct *_SetElm_ *prev;
+    struct *_SetElm_ *next;
+  } SetElm;
+
+  typedef struct _Set_ {
+    int size;
+    int (*match)(const void *key1, const void *key2);
+    void (*destory)(void *data);
+    SetElm *head;
+    SetElm *tail;
+  } Set;
+
+  void set_new(Set *set, int (*match)(const void *key1, const void *key2), void (*destory)(void *data));
+  void set_destory(Set *set);
+  int set_insert(Set *set, void *data);
+  int set_remove(Set *set, void **data);
+  int set_union(Set *setu, const Set *set1, const *set2);
+  int set_intersection(Set *seti, const Set *set1, const *set2);
+  int set_difference(Set *setd, const Set *set1, const *set2);
+  int set_is_member(const Set *set, const void *data);
+  int set_is_subset(const Set *set1, const Set *set2);
+  int set_is_equal(const Set *set1, const Set *set2);
+  #define set_size(set) ((set)->size)
+#endif
