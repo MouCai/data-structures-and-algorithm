da4a9c11056ec05bc7567b82d5c9e958f5d9d403
韦捷 <weijiewow@163.com>
2016-04-04 17:30:29 +0800

    bug fixed

--- set/set_is_subset.c
+++ set/set_is_subset.c
@@ -2,7 +2,7 @@
 
 int set_is_subset (const Set *set1, const Set *set2) {
   SetElm *member;
-  if (set_size(set1) >= set_size(set2))
+  if (set_size(set1) > set_size(set2))
     return 0;
   for (member = set_head(set1); member != NULL; member = set_next(member)) {
     if (set_is_member(set2, set_data(member))) 
