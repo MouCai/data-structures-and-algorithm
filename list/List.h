d08e114243dee1a30d8e9688f1b6fb30566c0ac7
韦捷 <weijiewow@163.com>
2016-04-05 21:30:49 +0800

    rewrited list.h

--- list/List.h
+++ list/List.h
@@ -1,35 +1,35 @@
-#ifndef _DOUBLY_LINKED_LIST_
-  #define _DOUBLY_LINKED_LIST_
+#ifndef _LIST_H_
+  #define _LIST_H_
   #include <stdlib.h>
-  struct List {
-     struct MNode *Head;
-     struct MNode *Tail;
-     struct MNode *LastNode;
-     unsigned long Length;
-  };
+  #include <string.h>
+  typedef struct _ListElm_ {
+    void *data;
+    struct _ListElm_ *prev;
+    struct _ListElm_ *next;
+  } ListElm;
+
+  typedef struct _List_ {
+    int size;
+    int (*match)(const void *key1, const void *key2);
+    void (*destory)(void *data);
+    ListElm *head;
+    ListElm *tail;
+  } List;
   
-  struct MNode {
-     struct MNode *Succ;
-     struct MNode *Pred;
-  };
-  
-  typedef struct MNode *NODE;
-  typedef struct List *LIST;
-  
-  LIST NewList(void);
-  NODE Append(LIST, NODE);
-  NODE Prepend(LIST, NODE);
-  NODE DeleteHead(LIST);
-  NODE DeleteTail(LIST);
-  NODE InsertAfter(LIST, NODE, NODE);
-  NODE DeleteNode(LIST, NODE);
-  void Destory(LIST);
-  
-  #define GetTail(LIST) ((LIST)->LastNode)
-  #define GetHead(LIST) ((LIST)->Head)
-  #define IsEmpty(LIST) ((LIST)->Head->Succ == NULL)
-  #define Length(LIST) ((LIST)->Length)
-  #define Pop DeleteTail
-  #define Shift DeleteHead
-  #define Push Append
+  void list_init(List *list, void (*destory)(void *data), int (*match)(const void *key1, const void *key2));
+  void list_destory(List *list);
+  int list_insert_next(List *list, ListElm *elm, const void *data);
+  int list_insert_prev(List *list, ListElm *elm, const void *data);
+  int list_remove(List *list, ListElm *elm, void **data);
+  int list_push(List *list, ListElm *elm);
+  ListElm list_pop(List *list);
+  ListElm list_shift(List *list);
+
+  #define list_tail(List) ((List)->tail)
+  #define list_head(List) ((List)->head)
+  #define list_is_empty(List) ((List)->head->next == NULL)
+  #define list_size(List) ((List)->size)
+  #define list_next(ListElm) ((ListElm)->next)
+  #define list_prev(ListElm) ((ListElm)->prev)
+  #define list_data(ListElm) ((ListElm)->data)
 #endif
