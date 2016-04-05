dcb23911b419fb93c1fb46da7901f5e34591ed05
韦捷 <weijiewow@163.com>
2016-04-05 22:46:43 +0800

    add list_insert_prev

---
+++ list/list_insert_prev.c
@@ -0,0 +1,30 @@
+#include "list.h"
+
+int list_insert_prev(List *list, ListElm *elm, const void *data) {
+  ListElm *new;
+
+  if (elm == NULL && list_size(list) != 0)
+    return -1;
+  if ((new = (ListElm *)malloc(sizeof(ListElm))) == NULL) 
+    return -1;
+
+  new->data = (void *)data;
+
+  if (list_size(list) == 0) {
+    list->head = new;
+    list->tail = new;
+    new->next = NULL;
+    new->prev = NULL;    
+  } else {
+    new->next = elm;
+    new->prev = elm->prev;
+    if (elm->prev == NULL)
+      list->head = new;
+    else
+      elm->prev->next = elm;
+    elm->prev = new;
+  }
+
+  list->size++;
+  return 0;
+}
--- list/main.c
+++ list/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "list.h"
+static int _match (const void *key1, const void *key2) {
+  return (*(int *)key1) == (*(int *)key2);
+}
 
 int main(int argc, char **argv) {
   // test list_init
   List *testList1 = malloc(sizeof(List));
-  list_init(testList1, NULL, NULL);
+  list_init(testList1, NULL, _match);
   printf("'list_init' is pass ? %d \n", testList1->tail == NULL &&
                                         testList1->head == NULL &&
                                         testList1->size == 0); 
@@ -15,9 +18,16 @@ int main(int argc, char **argv) {
   printf("'list_insert_next' is pass ? %d \n", (*(int *)list_data(testList1->tail)) == 1);
 
   // test list_push
-  int testData = 2;
-  testPtr1 = &testData;
+  int testData2 = 2;
+  testPtr1 = &testData2;
   list_push(testList1, testPtr1);
   printf("'list_push' is pass ? %d \n", (*(int *)list_data(testList1->tail)) == 2);
+
+  // test list_insert_prev
+  int testData3 = 3;
+  testPtr1 = &testData3;
+  list_insert_prev(testList1, testList1->tail, testPtr1);
+  printf("'list_insert_prev' is pass ? %d \n", (*(int *)list_data(testList1->tail->prev)) == 3);
+
   return 0;
 }
