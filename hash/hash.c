e0a89fa2d00fd5272dad05dd028bfadb2bdc4224
韦捷 <weijiewow@163.com>
2016-04-06 23:18:44 +0800

    add hash function

---
+++ hash/hash.c
@@ -0,0 +1,19 @@
+#include "hash.h"
+unsigned int hash_pjw(const void *key) {
+  const char *ptr;
+  unsigned int val;
+  
+  val = 0;
+  ptr = key;
+
+  while (*ptr != '\0') {
+    unsigned int temp;
+    val = (val << 4) + (*ptr);
+    if (temp = (val & 0xf0000000)) {
+      val = val ^ (temp >> 24);
+      val = val ^ temp;
+    }
+    ptr++;
+  }
+  return val;
+}
