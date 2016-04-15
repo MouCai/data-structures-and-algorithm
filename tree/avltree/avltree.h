41ccc694d1c586cb3b40a504a4f61e1ef9044cc2
韦捷 <weijiewow@163.com>
2016-04-16 02:38:02 +0800

    init avltree/

---
+++ tree/avltree/avltree.h
@@ -0,0 +1,24 @@
+#ifndef _AVLTREE_H_
+  #define _AVLTREE_H_
+  #include "../bitree/bitree.h"
+  
+  #define AVL_LEFT_HEAVY 1
+  #define AVL_BALANCED 0
+  #define AVL_RIGHT_HEAVY -1
+
+  typedef struct _AvlTreeNode_ {
+    void *data;
+    int hidden;
+    int factor;
+  } AvlTreeNode;
+
+  typedef AvlTree Bitree;
+
+  void avltree_destory(AvlTree *tree);
+  int avltree_insert(AvlTree *tree, const void *data);
+  int avltree_remove(AvlTree *tree, const void *data);
+  int avltree_lookup(AvlTree *tree, void **data);
+
+  #define avltree_init bitree_init
+  #define avltree_size(tree) ((tree)->size)
+#endif
---
+++ tree/avltree/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "bistree.h"
+int main (int argc, char **argv) {
+  return 0;
+}
