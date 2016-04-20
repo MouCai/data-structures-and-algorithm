8701d9b04e5ec07e1cf50191542d2dc512824b44
韦捷 <weijiewow@163.com>
2016-04-21 02:36:17 +0800

    add static rotate_right in to avltree_insert.c

--- tree/avltree/avltree_insert.c
+++ tree/avltree/avltree_insert.c
@@ -35,7 +35,38 @@ static void rotate_left(BiTreeNode **node) {
   return;
 }
 
-static void rotate_right(BiTreeNode *node) {
+static void rotate_right(BiTreeNode **node) {
+  BitreeNode *right, *grandchild;
+  right = bitree_right(node);
+  if (((AvlTreeNode *)bitree_data(left))->factor == AVL_RIGHT_HEAVY) {
+    bitree_right(*node) = bitree_left(right);
+    bitree_left(right) = *node;
+    ((AvlTreeNode *)bitree_data(node))->factor = AVL_BALANCED;
+    ((AvlTreeNode *)bitree_data(right))->factor = AVL_BALANCED;
+    *node = right;
+  } else {
+    grandchild = bitree_left(right);
+    bitree_left(right) = bitree_right(grandchild)
+    bitree_right(grandchild) = right;
+    bitree_right(node*) = bitree_left(grandchild)
+    bitree_left(grandchild) = *node;
+    switch (((AvlTreeNode *)bitree_data(grandchild))->factor) {
+      case AVL_LEFT_HEAVY:
+        ((AvlTreeNode *)bitree_data(node))->factor = AVL_BALANCED;
+        ((AvlTreeNode *)bitree_data(right))->factor = AVL_RIGHT_HEAVY;
+        break;
+      case AVL_BALANCED:
+        ((AvlTreeNode *)bitree_data(node))->factor = AVL_BALANCED;
+        ((AvlTreeNode *)bitree_data(right))->factor = AVL_BALANCED;
+        break;
+      case AVL_RIGHT_HEAVY:
+        ((AvlTreeNode *)bitree_data(node))->factor = AVL_LEFT_HEAVY;
+        ((AvlTreeNode *)bitree_data(right))->factor = AVL_BALANCED;
+        break;
+    }
+    ((AvlTreeNode *)bitree_data(grandchild))->factor = AVL_BALANCED;
+    *node = grandchild;
+  }
   return;
 }
 
