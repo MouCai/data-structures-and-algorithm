2aacd144bdf6f05b578ba9c7fff1f1f68ae82575
韦捷 <weijiewow@163.com>
2016-04-22 02:15:50 +0800

    fin avltree_insert

--- tree/avltree/avltree_insert.c
+++ tree/avltree/avltree_insert.c
@@ -70,6 +70,101 @@ static void rotate_right(BiTreeNode **node) {
   return;
 }
 
+static int insert(Bitree *tree, BitreeNode **node, const void *data, int *balanced) {
+  AvlTreeNode *avlnode;
+  int result, compare;
+  if (bitree_is_eob(node)) {
+    if (avlnode = (AvlTreeNode *)malloc(sizeof(AvlTreeNode)) == NULL) 
+      return -1;
+    avlnode->factor = 0;
+    avlnode->data = (void *)data;
+    avlnode->hidden = 0;
+    return bitree_insert_left(tree, *node, avlnode);
+  } else {
+    compare = tree->compare(data, ((AvlTreeNode *)bitree_data(node))->data);
+
+    if (compare < 0) {
+
+      if (bitree_is_eob(bitree_left(*node))) {
+        if (avlnode = (AvlTreeNode *)malloc(sizeof(AvlTreeNode)) == NULL) 
+          return -1;
+        avlnode->factor = 0;
+        avlnode->data = (void *)data;
+        avlnode->hidden = 0;
+        if (bitree_insert_left(tree, *node, avlnode) != 0)
+          return -1;
+        *balanced = 0;
+      } else {
+        if ((result = insert(tree, &bitree_left(*node), data, balanced)) != 0) {
+          return result;
+        }
+      }
+
+      if (!(*balanced)) {
+        switch (((AvlTreeNode *)bitree_data(*node))->factor) {
+          case AVL_LEFT_HEAVY:
+            rotate_left(node);
+            *balanced = 1;
+            break;
+          case AVL_BALANCED:
+            ((AvlTreeNode *)bitree_data(*node))->factor = AVL_LEFT_HEAVY;
+            break;
+          case AVL_RIGHT_HEAVY:
+            ((AvlTreeNode *)bitree_data(*node))->factor = AVL_BALANCED;
+            *balanced = 1;
+            break;
+        }
+      }
+
+    } else if (compare > 0) {
+      
+      if (bitree_is_eob(bitree_right(*node))) {
+        if (avlnode = (AvlTreeNode *)malloc(sizeof(AvlTreeNode)) == NULL) 
+          return -1;
+        avlnode->factor = 0;
+        avlnode->data = (void *)data;
+        avlnode->hidden = 0;
+        if (bitree_insert_right(tree, *node, avlnode) != 0)
+          return -1;
+        *balanced = 0;
+      } else {
+        if ((result = insert(tree, &bitree_right(*node), data, balanced)) != 0) {
+          return result;
+        }
+      }
+
+      if (!(*balanced)) {
+        switch (((AvlTreeNode *)bitree_data(*node))->factor) {
+          case AVL_LEFT_HEAVY:
+            ((AvlTreeNode *)bitree_data(*node))->factor = AVL_BALANCED;
+            *balanced = 1;
+            break;
+          case AVL_BALANCED:
+            ((AvlTreeNode *)bitree_data(*node))->factor = AVL_RIGHT_HEAVY;
+            break;
+          case AVL_RIGHT_HEAVY:
+            rotate_right(*node);
+            *balanced = 1;
+            break;
+        }
+      }
+
+    } else {
+      if (!((AvlTreeNode *)bitree_data(*node))->hidden) {
+        return 1
+      } else {
+        if (tree->destory != NULL)
+          tree->destory(((AvlTreeNode *)bitree_data(*node))->data);
+        ((AvlTreeNode *)bitree_data(*node))->data = (void *data);
+        ((AvlTreeNode *)bitree_data(*node))->hidden = 0;
+        *balanced = 1;
+      }
+      return 0;
+    }
+  }
+}
+
 int avltree_insert(AvlTree *tree, const void *data) {
-  return 0;
+  int balanced = 0;
+  return insert(tree, &bitree_root(tree), data, &balanced);
 }
