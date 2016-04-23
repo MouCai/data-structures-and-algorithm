f70692bdf037991d647e7b167d1ee0e9d28eb522
韦捷 <weijiewow@163.com>
2016-04-23 17:46:36 +0800

    add avltree_remove

--- tree/avltree/avltree_remove.c
+++ tree/avltree/avltree_remove.c
@@ -1,5 +1,21 @@
 #include "avltree.h"
 
-int avltree_remove(AvlTree *tree, const void *data) {
-  return 0;
+static int hide(BiTree *tree, BiTreeNode *node, const void *data) {
+  int compare, result;
+  if (bitree_is_eob(node))
+    return -1;
+  compare = tree->compare(data, (AvlTreeNode *)(bitree_data(node))->data);
+  if (compare < 0) {
+    result = hide(tree, bitree_left(node), const void *data);
+  } else if (compare > 0) {
+    result = hide(tree, bitree_right(node), const void *data);
+  } else {
+    (AvlTreeNode *)(bitree_data(node))->hidden = 1;
+    result = 0;
+  }
+  return result;
+}
+
+int avltree_remove(BiTree *tree, const void *data) {
+  return hide(tree, bitree_root(tree), data);
 }
