99cfefd8042b5e0a75d50ac3b3afd47f68f4cf28
韦捷 <weijiewow@163.com>
2016-05-09 16:31:20 +0800

    fin graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -17,4 +17,17 @@
     List Adjlists;
   } Graph;
 
+  typedef enum _VertexColor_ {white, gray, black} VertexColor;
+  void graph_init(Graph *graph, int (match *)(const void *key1, const void *key2), void (destory *)(void *data));
+  void graph_destory(Graph *graph);
+  int graph_insert_vertex(Graph *graph, const void *data);
+  int graph_insert_edge(Graph *graph, const void *data1, const void *data2);
+  int graph_remove_vertex(Graph *graph, void **data);
+  int graph_remove_edge(Graph *graph, void *data1, void **data2);
+  int graph_adjlist(Graph *graph, const void *data, Adjlists *adjlist);
+  int graph_is_adjacent(const Graph *graph, const void *data1, const void *data2);
+
+  #define graph_adjlists(graph)((graph)->adjlists)
+  #define graph_vcount(graph)((graph)->vcount)
+  #define graph_ecount(graph)((graph)->ecount)
 #endif
