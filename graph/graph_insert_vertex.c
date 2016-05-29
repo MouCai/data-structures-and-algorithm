de84e48b140cc25aebb596e3b1390576b312f495
韦捷 <weijiewow@163.com>
2016-05-29 17:42:19 +0800

    add gtaph_insert_vertex.c

---
+++ graph/graph_insert_vertex.c
@@ -0,0 +1,25 @@
+#include "graph.h"
+
+int graph_insert_vertex(Graph *graph, const void *data) {
+  ListElm *element;
+  AdjList *adjlist;
+
+  int result;
+
+  for (element = list_head(graph->adjlist); element != NULL; element = list_next(element)) {
+    if (graph->match(data, (AdjList *)list_data(element)->vertex)) 
+      return 1;
+  }
+
+  if ((adjlist = (AdjList *)malloc(sizeof(AdjList))) == NULL)
+    return -1;
+
+  adjlist->vertex = (void *)data;
+  set_init(&adjlist->adjacent, graph->match, NULL);
+  
+  if ((result = list_push(&graph->adjlists, adjlist)) != 0) {
+    return result;
+  }
+  graph->vcount++;
+  return 0;
+}
